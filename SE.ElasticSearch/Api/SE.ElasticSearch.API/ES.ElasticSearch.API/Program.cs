3308b9a994b19f6916d1b9f8b027fd18b146d914
Semih Esentürk <semihesenturk@outlook.com>
2025-02-08 21:54:54 +0300

    ElasticSearch package NEST has installed

--- SE.ElasticSearch/Api/SE.ElasticSearch.API/ES.ElasticSearch.API/Program.cs
+++ SE.ElasticSearch/Api/SE.ElasticSearch.API/ES.ElasticSearch.API/Program.cs
@@ -1,10 +1,13 @@
+using ES.ElasticSearch.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//Add ElasticSearch
+builder.Services.AddElastic(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
