5a41bf0c458a2a9b40095c8d33fa21b8f1d87120
Semih Esentürk <semihesenturk@outlook.com>
2025-02-12 16:16:28 +0300

    TermQuery ways added to repository
    TermQuery ways added to repository

--- ES.ElasticSearch/Api/ES.ElasticSearch.API/ES.ElasticSearch.API/Repositories/ECommerceRepository.cs
+++ ES.ElasticSearch/Api/ES.ElasticSearch.API/ES.ElasticSearch.API/Repositories/ECommerceRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Immutable;
 using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 using ES.ElasticSearch.API.Models.ECommerceModel;
 
 namespace ES.ElasticSearch.API.Repositories;
@@ -10,11 +11,33 @@ public class ECommerceRepository(ElasticsearchClient client)
 
     public async Task<ImmutableList<ECommerce>> TermQuery(string customerFirstName)
     {
+        //First way to search
+        // var result = await client.SearchAsync<ECommerce>(
+        //     s => s.Index(IndexName).Query(
+        //         q => q.Term(
+        //             t => t.Field("customer_first_name.keyword"!).Value(customerFirstName)
+        //         )));
+
+        // Second way to search
         var result = await client.SearchAsync<ECommerce>(
-            s => s.Index(IndexName).Query(
-                q => q.Term(
-                    t => t.Field("customer_first_name.keyword"!).Value(customerFirstName)
-                )));
+            s => s.Index(IndexName)
+                .Query(
+                    q => q.Term(
+                        t => t.Field(f => f.CustomerFirstName.Suffix("keyword"))
+                            .Value(customerFirstName))
+                ));
+
+        //Another way to search
+        // var termQuery = new TermQuery("customer_first_name.keyword"!)
+        // {
+        //     Value = customerFirstName,
+        //     CaseInsensitive = true
+        // };
+        //
+        // var result = await client.SearchAsync<ECommerce>(
+        //     s => s.Index(IndexName)
+        //         .Query(termQuery)
+        // );
 
         foreach (var hit in result.Hits) hit.Source.Id = hit.Id;
         return result.Documents.ToImmutableList();
